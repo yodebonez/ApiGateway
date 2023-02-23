6ba02d689fe8f83b6709379d958166f3fcc716e0
ojo olayode <yodeboy@gmail.com>
2023-02-23 02:39:35 +0100

    Ocelot api gateway config set up

---
+++ CustodianApiGateway/Program.cs
@@ -0,0 +1,42 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// cors
+builder.Services.AddCors(options => {
+    options.AddPolicy("CORSPolicy", builder => builder.AllowAnyMethod().AllowAnyHeader().AllowCredentials().SetIsOriginAllowed((hosts) => true));
+});
+
+//ocelot configuration
+builder.Services.AddOcelot(builder.Configuration);
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+
+.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+.AddEnvironmentVariables();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await app.UseOcelot();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
